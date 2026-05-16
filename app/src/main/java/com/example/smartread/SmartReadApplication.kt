c456abc73fb472ade92a259fb46c1d9a7cfbb89f
myyyhhh <2312252@mail.nankai.edu.cn>
2026-05-16 21:49:37 +0800

    基本页面显示,修改AndroidManifest.xml的主界面为SmartReadApplication

--- app/src/main/java/com/example/smartread/SmartReadApplication.kt
+++ app/src/main/java/com/example/smartread/SmartReadApplication.kt
@@ -3,49 +3,16 @@ package com.example.smartread
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.smartread.presentation.reader.MainAppScaffold
 import com.example.smartread.ui.theme.SmartReadTheme
 
 class SmartReadApplication : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SmartReadTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    // 调用主页面+底部导航
-                    MainAppScaffold()
-                }
+                MainAppScaffold() // 调用你的导航
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartReadTheme {
-        Greeting("Android")
     }
 }
